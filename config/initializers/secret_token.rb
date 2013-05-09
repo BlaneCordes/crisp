c4e3fcdce601590ddff78c9e8797332dff99bc41
Blane Cordes <blane.cordes@echostar.com>
2013-05-09 17:35:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crisp::Application.config.secret_token = 'd5b92a59c100661c12454bb47512964a880efd2b55105ad7a137f0fbf83e017d6491db6d92faf8421b9496dd0e484be966af4ae2f8a7a926cb15e7950a061437'
